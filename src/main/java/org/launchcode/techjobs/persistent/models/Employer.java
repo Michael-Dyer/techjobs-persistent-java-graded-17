2d44c9629f42d62fc65d9fe703a185c2c416744d
Michael-Dyer <Dyerm636@yahoo.com>
2024-11-15 20:03:27 -0600

    still need to update homecontroller on pt3

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -16,6 +21,9 @@ public class Employer extends AbstractEntity {
     private String location;
 
 
+    @OneToMany(mappedBy = "employer_id")
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
 
     public @NotBlank @Size(min = 2, max = 150) String getLocation() {
